f5c75dcacb96d0013a6d2aa4000db4608e8d7167
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-21 16:45:25 -0300

    feat: add Header, InputWithLabel, Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { useForm } from 'react-hook-form'
+import Header from './components/Header'
+import InputWithLabel from './components/InputWithLabel'
+import Button from './components/Button'
 import { SubmitFormParams } from './dtos/CreateUser'
+import { UserPlus } from 'react-feather'
 import logoGrowth from './assets/logo.png'
 import './styles/global.css'
 
 const App: FC = () => {
-  const [stateMenuButton, setStateMenuButton] = useState(false)
-
   const { register, handleSubmit } = useForm()
-
-  function handleMenuButton() {
-    setStateMenuButton(!stateMenuButton)
-  }
 
   const handleSubmitForm = async (data: SubmitFormParams) => {
     console.log(data)
@@ -20,107 +18,68 @@ const App: FC = () => {
   return (
     <div className='container'>
       <img src={logoGrowth} alt='Growth tech logo'/>
-      <header className='header'>
-        <button type='button' className='menu-button' onClick={handleMenuButton}>
-          Menu
-          <div className='menu-icon'>
-            <span className='first-bar'></span>
-            <span className='second-bar'></span>
-            <span className='third-bar'></span>
-          </div>
-        </button>
-        {stateMenuButton ? (<nav>
-          <ul>
-            <li>
-              <a href='/user'>Cadastrar usuário</a>
-            </li>
-            <li>
-              <a href='/user/post'>Criar post</a>
-            </li>
-            <li>
-              <a href='/allPost'>Listar post</a>
-            </li>
-          </ul>
-        </nav>) : null}
-      </header>
+      <Header />
       <main>
         <h2 className='title-secundary'>Formulário para cadastro de usuário</h2>
         <form className='form-user' onSubmit={handleSubmit(handleSubmitForm)}>
-          <label htmlFor='name'>Nome completo</label>
-          <input 
-            type='text' 
-            placeholder='Nome completo' 
+          <InputWithLabel
             name='name'
-            ref={register({ required: true })}
+            labelAndPlaceholder='Nome completo'
+            refForm={register({ required: true})}
           />
 
-          <label htmlFor='username'>Nome de usuário</label>
-          <input 
-            type='text' 
-            placeholder='Nome de usuário' 
+          <InputWithLabel
             name='username'
-            ref={register({ required: true })} 
+            labelAndPlaceholder='Nome de usuário'
+            refForm={register({ required: true})}
           />
 
-          <label htmlFor='email'>Email</label>
-          <input 
-            type='email' 
-            placeholder='Email' 
+          <InputWithLabel
             name='email'
-            ref={register({ required: true })}
+            labelAndPlaceholder='Email'
+            type='email'
+            refForm={register({ required: true})}
           />
 
-          <label htmlFor='phone'>Número de celular</label>
-          <input 
-            type='text' 
-            placeholder='Número de celular' 
+          <InputWithLabel
             name='phone'
-            ref={register({ required: true })}
+            labelAndPlaceholder='Número de celular'
+            refForm={register({ required: true})}
           />
 
-          <label htmlFor='street'>Nome da rua</label>
-          <input
-            type='text'
-            placeholder='Nome da rua'
+          <InputWithLabel
             name='street'
-            ref={register({ required: true })}
+            labelAndPlaceholder='Nome da rua'
+            refForm={register({ required: true})}
           />
 
-          <label htmlFor='suite'>Número da casa/apartamento</label>
-          <input
-            type='text'
-            placeholder='Número da casa/apartamento'
+          <InputWithLabel
             name='suite'
-            ref={register({ required: true })}
+            labelAndPlaceholder='Número da casa/apartamento'
+            refForm={register({ required: true})}
           />
 
-          <label htmlFor='city'>Nome da cidade</label>
-          <input
-            type='text'
-            placeholder='Nome da cidade'
+          <InputWithLabel
             name='city'
-            ref={register({ required: true })}
+            labelAndPlaceholder='Nome da cidade'
+            refForm={register({ required: true})}
           />
-          
-          <label htmlFor='zipcode'>CEP</label>
-          <input
-            type='text'
-            placeholder='Nome da rua'
+
+          <InputWithLabel
             name='zipcode'
-            ref={register({ required: true })}
+            labelAndPlaceholder='CEP'
+            refForm={register({ required: true})}
           />
 
-          <label htmlFor='companyName'>Nome da empresa</label>
-          <input
-            type='text'
-            placeholder='Nome da empresa'
+          <InputWithLabel
             name='companyName'
-            ref={register({ required: true })}
+            labelAndPlaceholder='Nome da empresa'
+            refForm={register({ required: true})}
           />
 
-          <button type='submit' className='submit-button'>
+          <Button type='submit' icon={UserPlus}>
             Cadastrar
-          </button>
+          </Button>
         </form>
       </main>
     </div>
