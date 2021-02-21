66f2a75d9f2948ef0aae47a133f74b042d197c1b
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-21 18:42:01 -0300

    feat: add Response

--- src/dtos/CreateUser/index.ts
+++ src/dtos/CreateUser/index.ts
@@ -16,3 +16,37 @@ export type SubmitFormParams = {
   address: Address
   company: Company
 }
+
+type ResponseDataGeolocation = {
+	lat: string
+	lng: string
+}
+
+type ResponseDataAddress = {
+	street: string
+	suite: string
+	city: string
+	zipcode: string
+	geo: ResponseDataGeolocation
+}
+
+type ResponseDataCompany = {
+	name: string
+	catchPhase: string
+	bs: string
+}
+
+type UserHttpResponse = {
+  id: number
+	name: string
+	username: string
+	email: string
+	phone: string
+	website: string
+	address: ResponseDataAddress
+	company: ResponseDataCompany
+}
+
+export type Response = {
+  data: UserHttpResponse
+}
